a25573ef6f1565dd86dd726cb04a5ce7de877bdf
Ile <ilmari.marttila@gmail.com>
2022-10-05 00:57:30 +0300

    Add user table to database

---
+++ src/database/migrations/20221004193118_create_user_table.ts
@@ -0,0 +1,19 @@
+import { Knex } from 'knex';
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.createTable('user', (table) => {
+    table.increments('id');
+    table
+      .string('email', 254) // https://www.rfc-editor.org/errata_search.php?rfc=3696
+      .notNullable();
+    table.string('forename', 255).notNullable();
+    table.string('surname', 255).notNullable();
+    table.string('salt', 128).notNullable();
+    table.string('password_hash', 255).notNullable();
+    table.timestamps(true, true);
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.dropTable('user');
+}
