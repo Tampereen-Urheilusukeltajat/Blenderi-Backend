b1c34d2006f6de5e9daafbf4e6a620e5d8c6d709
Ile <ilmari.marttila@gmail.com>
2022-10-05 04:07:08 +0300

    POC: Add new user to database

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,5 +1,6 @@
 import { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify';
 import { Type, Static } from '@sinclair/typebox';
+import { knexController } from '../database/database';
 
 const userAddedResponse = Type.Object({
   status: Type.String(),
@@ -49,9 +50,32 @@ const handler = async (
   req: FastifyRequest,
   reply: FastifyReply
 ): Promise<void> => {
-  // TODO: validate and verify stuff
-  reply.code(201);
-  await reply.send();
+  if (req.body) {
+    // it has it believe or not...
+    const result = await knexController('user')
+      .count()
+      .where('email', req.body['email']);
+    const count: Number = Number(result[0]['count(*)']);
+    if (count > 0) {
+      reply.code(409);
+      await reply.send();
+    } else {
+      const body: any = req.body;
+      console.log('foo');
+      await knexController('user').insert({
+        email: body['email'] as string,
+        forename: body['forename'] as string,
+        surname: body['surname'] as string,
+        admin: false,
+        blender: false,
+        salt: 'suolaa haavoihin',
+        password_hash: 'hassi',
+      });
+      console.log('bar');
+      reply.code(201);
+      await reply.send();
+    }
+  }
 };
 
 export default async (fastify: FastifyInstance): Promise<void> => {
