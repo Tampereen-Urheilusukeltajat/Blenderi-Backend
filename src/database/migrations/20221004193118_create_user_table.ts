e6e2f47db894c5601aea4e5c89d9b2ce335e8709
Ile <ilmari.marttila@gmail.com>
2022-10-06 16:34:30 +0300

    Autogenerate user id

--- src/database/migrations/20221004193118_create_user_table.ts
+++ src/database/migrations/20221004193118_create_user_table.ts
@@ -2,7 +2,7 @@ import { Knex } from 'knex';
 
 export async function up(knex: Knex): Promise<void> {
   await knex.schema.createTable('user', (table) => {
-    table.uuid('id').primary();
+    table.uuid('id').defaultTo(knex.raw('UUID()')).primary();
     table.string('email', 254); // https://www.rfc-editor.org/errata_search.php?rfc=3696
     table.string('forename', 255);
     table.string('surname', 255);
