cbcbc968f56ab4b9431a82f05ff61de1c1339156
Ile <ilmari.marttila@gmail.com>
2022-10-13 00:40:33 +0300

    Add tank sets with 1 to many bottles

---
+++ src/database/migrations/20221012211534_create_diving_tank_set_tables.ts
@@ -0,0 +1,35 @@
+import { Knex } from 'knex';
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.createTable('diving_cylinder', (table) => {
+    table.uuid('id').primary();
+    table.integer('volume').notNullable();
+    table.integer('pressure').notNullable();
+    table.string('material', 32).notNullable();
+    table.string('serial_number', 64).notNullable();
+    table.date('inspection').notNullable();
+    table.timestamps(true, true);
+  });
+
+  await knex.schema.createTable('tank_set', (table) => {
+    table.uuid('id').primary();
+    table.uuid('owner').references('id').inTable('user').notNullable();
+    table.string('name', 255).unique().notNullable();
+    table.timestamps(true, true);
+  });
+
+  await knex.schema.createTable('tank_set_diving_cylider', (table) => {
+    table
+      .uuid('cylinder')
+      .references('id')
+      .inTable('diving_cylinder')
+      .primary();
+    table.uuid('tank_set').references('id').inTable('tank_set').primary();
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.dropTable('tank_set_diving_cylinder');
+  await knex.schema.dropTable('tank_set');
+  await knex.schema.dropTable('diving_cylinder');
+}
