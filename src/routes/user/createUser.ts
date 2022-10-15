07893bd7d684b0bd920da62f8b1e70387a78022b
Akseli Kolari <akzuu@pm.me>
2022-10-15 15:52:38 +0300

    Final touches

--- src/routes/user/createUser.ts
+++ src/routes/user/createUser.ts
@@ -7,7 +7,6 @@ import {
   CreateUserRequest,
 } from '../../types/user.types';
 import { hashPassword } from '../../lib/auth';
-import { log } from '../../lib/log';
 
 const schema = {
   description: 'Creates a user',
@@ -38,7 +37,6 @@ const handler = async (
     .then((row: { 'count(`email`)': number }) => Number(row['count(`email`)']));
 
   if (emailCount > 0) {
-    log.info('Tried to create user with duplicate email');
     return reply.code(409).send({
       statusCode: 409,
       error: 'Conflict',
