edb2f312fcb413e101955cc176a1bea08fae1dd4
Ile <ilmari.marttila@gmail.com>
2022-10-19 14:50:13 +0300

    Separate sections for happy and unhappy paths

--- src/test/routes/cylinder-set/updateCylinderSet.test.ts
+++ src/test/routes/cylinder-set/updateCylinderSet.test.ts
@@ -19,108 +19,112 @@ describe('update cylinder set', () => {
     await knexController.destroy();
   });
 
-  test('it responds with 200 and proper body if update was successful; no cylinders updated', async () => {
-    const payload = {
-      name: 'bottlename',
-    };
+  describe('happy paths', () => {
+    test('it responds with 200 and proper body if update was successful; no cylinders updated', async () => {
+      const payload = {
+        name: 'bottlename',
+      };
 
-    const server = await getTestInstance();
-    const res = await server.inject({
-      url: 'api/cylinder-set/f4e1035e-f36e-4056-9a1b-5925a3c5793e',
-      method: 'PATCH',
-      payload,
+      const server = await getTestInstance();
+      const res = await server.inject({
+        url: 'api/cylinder-set/f4e1035e-f36e-4056-9a1b-5925a3c5793e',
+        method: 'PATCH',
+        payload,
+      });
+
+      expect(res.statusCode).toEqual(200);
+      const responseBody = JSON.parse(res.body);
+      expect(responseBody.name).toEqual(payload.name);
     });
 
-    expect(res.statusCode).toEqual(200);
-    const responseBody = JSON.parse(res.body);
-    expect(responseBody.name).toEqual(payload.name);
-  });
+    test('it responds with 200 and proper body if update of multiple cylinder in a set was successful', async () => {
+      const payload = {
+        cylinders: [
+          {
+            id: '1e54c95c-c2fe-4d86-9406-c88f45c0bde9',
+            pressure: 232,
+            inspection: '2022-01-01',
+          },
+          {
+            id: '362ab1b8-4f88-11ed-aa8f-a7bc5ca309a3',
+            inspection: '2021-01-03',
+          },
+        ],
+      };
 
-  test('it responds with 200 and proper body if update of multiple cylinder in a set was successful', async () => {
-    const payload = {
-      cylinders: [
-        {
-          id: '1e54c95c-c2fe-4d86-9406-c88f45c0bde9',
-          pressure: 232,
-          inspection: '2022-01-01',
-        },
-        {
-          id: '362ab1b8-4f88-11ed-aa8f-a7bc5ca309a3',
-          inspection: '2021-01-03',
-        },
-      ],
-    };
+      const server = await getTestInstance();
+      const res = await server.inject({
+        url: 'api/cylinder-set/f4e1035e-f36e-4056-9a1b-5925a3c5793e',
+        method: 'PATCH',
+        payload,
+      });
 
-    const server = await getTestInstance();
-    const res = await server.inject({
-      url: 'api/cylinder-set/f4e1035e-f36e-4056-9a1b-5925a3c5793e',
-      method: 'PATCH',
-      payload,
-    });
+      const responseBody = JSON.parse(res.body);
 
-    const responseBody = JSON.parse(res.body);
+      expect(res.statusCode).toEqual(200);
 
-    expect(res.statusCode).toEqual(200);
-
-    for (const cylinder of responseBody.cylinders) {
-      if (cylinder.id === '1e54c95c-c2fe-4d86-9406-c88f45c0bde9') {
-        expect(cylinder.pressure).toEqual(232);
-        expect(cylinder.inspection).toContain('2022-01-01');
+      for (const cylinder of responseBody.cylinders) {
+        if (cylinder.id === '1e54c95c-c2fe-4d86-9406-c88f45c0bde9') {
+          expect(cylinder.pressure).toEqual(232);
+          expect(cylinder.inspection).toContain('2022-01-01');
+        }
+        if (cylinder.id === '362ab1b8-4f88-11ed-aa8f-a7bc5ca309a3') {
+          expect(cylinder.inspection).toContain('2021-01-03');
+        }
       }
-      if (cylinder.id === '362ab1b8-4f88-11ed-aa8f-a7bc5ca309a3') {
-        expect(cylinder.inspection).toContain('2021-01-03');
-      }
-    }
+    });
   });
 
-  test('it responds with 404 if cylinder set is not found', async () => {
-    const payload = {
-      name: 'pullonnimi',
-      cylinders: [],
-    };
+  describe('unhappy paths', () => {
+    test('it responds with 404 if cylinder set is not found', async () => {
+      const payload = {
+        name: 'pullonnimi',
+        cylinders: [],
+      };
 
-    const server = await getTestInstance();
-    const res = await server.inject({
-      url: 'api/cylinder-set/05e3d142-4f79-11ed-a77d-7399a01cf8ab',
-      method: 'PATCH',
-      payload,
+      const server = await getTestInstance();
+      const res = await server.inject({
+        url: 'api/cylinder-set/05e3d142-4f79-11ed-a77d-7399a01cf8ab',
+        method: 'PATCH',
+        payload,
+      });
+
+      expect(res.statusCode).toEqual(404);
     });
 
-    expect(res.statusCode).toEqual(404);
-  });
+    test('it responds with 409 if same user tries to update cylinder set to have conflicting name', async () => {
+      const payload = {
+        name: 'bb',
+      };
 
-  test('it responds with 409 if same user tries to update cylinder set to have conflicting name', async () => {
-    const payload = {
-      name: 'bb',
-    };
+      const server = await getTestInstance();
+      const res = await server.inject({
+        url: 'api/cylinder-set/a4e1035e-f36e-4056-9a1b-5925a3c5793e',
+        method: 'PATCH',
+        payload,
+      });
 
-    const server = await getTestInstance();
-    const res = await server.inject({
-      url: 'api/cylinder-set/a4e1035e-f36e-4056-9a1b-5925a3c5793e',
-      method: 'PATCH',
-      payload,
+      expect(res.statusCode).toEqual(409);
     });
 
-    expect(res.statusCode).toEqual(409);
-  });
+    test('it responds with 400 for invalid body', async () => {
+      const payload = {
+        name: 'bottle5',
+        cylinders: [
+          {
+            id: '1e54c95c-c2fe-4d86-9406-c88f45c0bde9',
+            inspection: '🦀',
+          },
+        ],
+      };
+      const server = await getTestInstance();
+      const res = await server.inject({
+        url: 'api/cylinder-set/f4e1035e-f36e-4056-9a1b-5925a3c5793e',
+        method: 'PATCH',
+        payload,
+      });
 
-  test('it responds with 400 for invalid body', async () => {
-    const payload = {
-      name: 'bottle5',
-      cylinders: [
-        {
-          id: '1e54c95c-c2fe-4d86-9406-c88f45c0bde9',
-          inspection: '🦀',
-        },
-      ],
-    };
-    const server = await getTestInstance();
-    const res = await server.inject({
-      url: 'api/cylinder-set/f4e1035e-f36e-4056-9a1b-5925a3c5793e',
-      method: 'PATCH',
-      payload,
+      expect(res.statusCode).toEqual(400);
     });
-
-    expect(res.statusCode).toEqual(400);
   });
 });
