96ed0b645e7ed8cb08de6bf76d28a5b0dd67b522
Kimi Porthan <45666268+Tiikewi@users.noreply.github.com>
2022-10-21 22:24:23 +0300

    Endpoint: Update user tests (#9)
    Co-authored-by: Akseli Kolari <33838340+Akzuu@users.noreply.github.com>

---
+++ src/test/lib/auth.test.ts
@@ -0,0 +1,17 @@
+import { describe, test, expect } from '@jest/globals';
+import { hashPassword } from '../../lib/auth';
+
+describe('Hash password', () => {
+  test('hash is not plain password', async () => {
+    const testPass = 'testpass123';
+    const hashObj = await hashPassword(testPass);
+
+    // bcrpyt hash should always 60 characters long.
+    const hashLength = hashObj.hash.length;
+    expect(hashLength).toEqual(60);
+
+    expect(hashObj.hash).not.toEqual(testPass);
+    expect(hashObj.hash).not.toEqual(undefined);
+    expect(hashObj.salt).not.toEqual(undefined);
+  });
+});
