4d4b2709153cc689fab8c177abb0d51e20137561
Kimi <porthan.kimi@gmail.com>
2022-10-22 15:21:19 +0300

    first draft

---
+++ src/routes/cylinder-set/deleteCylinderSet.ts
@@ -0,0 +1,73 @@
+import { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify';
+import { knexController } from '../../database/database';
+import { errorHandler } from '../../lib/errorHandler';
+import { Type } from '@sinclair/typebox';
+import {
+  cylinderSetIdParamsPayload,
+  CylinderSetIdParamsPayload,
+} from '../../types/cylinderSet.types';
+
+const deleteSetReply = Type.Object({
+  setId: Type.String(),
+  deletedAt: Type.String(),
+});
+
+const schema = {
+  description: 'Deletes a diving cylinder set',
+  tags: ['Cylinder set', 'Delete'],
+  params: cylinderSetIdParamsPayload,
+  response: {
+    200: deleteSetReply,
+    404: { $ref: 'error' },
+    500: { $ref: 'error' },
+  },
+};
+
+const handler = async (
+  request: FastifyRequest<{
+    Params: CylinderSetIdParamsPayload;
+  }>,
+  reply: FastifyReply
+): Promise<void> => {
+  // TODO: Authorization
+
+  const setId = request.params.cylinderSetId;
+
+  await knexController.transaction(async (trx) => {
+    // Get id's of cylinders that belong to given set.
+    const cylinders: string[] = await trx
+      .select('cylinder')
+      .from('diving_cylinder_to_set')
+      .where('cylinder_set', setId);
+
+    // Delete single cylinders from set.
+    await trx('diving_cylinder_to_set')
+      .where('diving_cylinder_set', setId)
+      .del();
+
+    // Delete single cylinders.
+    for (const cylinder of cylinders) {
+      await trx('diving_cylinder').where('id', cylinder).del();
+    }
+
+    // Delete set.
+    const setResponse = await trx('diving_cylinder_set')
+      .where('diving_cylinder_set', setId)
+      .del();
+
+    if (setResponse === 0) {
+      return errorHandler(reply, 404, 'No set with given id found.');
+    }
+  });
+
+  await reply.code(200).send(deleteSetReply);
+};
+
+export default async (fastify: FastifyInstance): Promise<void> => {
+  fastify.route({
+    method: 'DELETE',
+    url: '/:cylinderSetId',
+    handler,
+    schema,
+  });
+};
