0cc0935fb5afcf67584fd98e452a2c135b855542
Kimi <porthan.kimi@gmail.com>
2022-10-23 21:34:07 +0300

    small fixes & add tests

--- src/routes/cylinder-set/deleteCylinderSet.ts
+++ src/routes/cylinder-set/deleteCylinderSet.ts
@@ -3,12 +3,13 @@ import { knexController } from '../../database/database';
 import { errorHandler } from '../../lib/errorHandler';
 import { Type } from '@sinclair/typebox';
 import {
+  Cylinder,
   cylinderSetIdParamsPayload,
   CylinderSetIdParamsPayload,
 } from '../../types/cylinderSet.types';
 
 const deleteSetReply = Type.Object({
-  setId: Type.String(),
+  setID: Type.String(),
   message: Type.String(),
 });
 
@@ -34,30 +35,35 @@ const handler = async (
 ): Promise<void> => {
   // TODO: Authorization
 
-  const setId = request.params.cylinderSetId;
+  const setID = request.params.cylinderSetId;
 
   await knexController.transaction(async (trx) => {
     // Get id's of cylinders that belong to given set.
     const rowData = await trx
       .select('cylinder')
-      .from('diving_cylinder_to_set')
-      .where('cylinder_set', setId);
+      .from<Cylinder>('diving_cylinder_to_set')
+      .where('cylinder_set', setID);
 
     if (rowData.length === 0) {
       return errorHandler(reply, 404, 'No set with given id found.');
     }
 
     // Delete cylinders from set.
-    await trx('diving_cylinder_to_set').where('cylinder_set', setId).del();
+    await trx('diving_cylinder_to_set').where('cylinder_set', setID).del();
 
     // Delete single cylinders.
-    await trx('diving_cylinder').whereIn('id', rowData).del();
+    // Knex queries return RowDataPacket obj's, and can't get them to work with whereIn :)
+    const cylinders: string[] = Object.values(
+      JSON.parse(JSON.stringify(rowData))
+    );
+
+    await trx.from('diving_cylinder').whereIn('id', cylinders).del();
 
     // Delete set.
-    await trx('diving_cylinder_set').where('id', setId).del();
+    await trx('diving_cylinder_set').where('id', setID).del();
   });
 
-  await reply.code(200).send({ setId, message: 'Set deleted successfully!' });
+  await reply.code(200).send({ setID, message: 'Set deleted successfully!' });
 };
 
 export default async (fastify: FastifyInstance): Promise<void> => {
