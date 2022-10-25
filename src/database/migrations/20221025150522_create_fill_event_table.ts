eb3be2dffdab3b11177a84046fffc031d3c7a84c
Henri Kiiski <henri.kiiski@tuni.fi>
2022-10-25 20:14:30 +0300

    Ensimmäinen iteraatio

---
+++ src/database/migrations/20221025150522_create_fill_event_table.ts
@@ -0,0 +1,39 @@
+import { Knex } from 'knex';
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.createTable('fill_event', (table) => {
+    table.increments('id');
+    table.uuid('user').references('id').inTable('user').notNullable();
+    table
+      .uuid('cylinder_set')
+      .references('id')
+      .inTable('diving_cylinder_set')
+      .notNullable();
+    table.timestamps(true, true);
+  });
+
+  await knex.schema.createTable('fill_cylinder_event', (table) => {
+    table
+      .increments('fill_event')
+      .references('id')
+      .inTable('fill_event')
+      .notNullable();
+    table
+      .uuid('cylinder')
+      .references('id')
+      .inTable('diving_cylinder')
+      .notNullable();
+    table
+      .string('gas', 15)
+      .checkIn(['pressurized_air', 'oxygen', 'helium', 'argon'])
+      .notNullable();
+    table.integer('fill_pressure').notNullable();
+    table.string('info', 2048); // arbitrary number
+    table.unique(['fill_event', 'cylinder']);
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.dropTable('fill_cylinder_event');
+  await knex.schema.dropTable('fill_event');
+}
