61c4854de12e88bdf46d9d350878b1b5460d60f9
toppeh <henri.kiiski@tuni.fi>
2022-10-27 14:47:36 +0300

    Kolmas iteraatio

--- src/database/migrations/20221025150522_create_fill_event_table.ts
+++ src/database/migrations/20221025150522_create_fill_event_table.ts
@@ -2,40 +2,23 @@ import { Knex } from 'knex';
 
 export async function up(knex: Knex): Promise<void> {
   await knex.schema.createTable('fill_event', (table) => {
-    table.increments('id');
+    table.uuid('id').primary();
     table.uuid('user').references('id').inTable('user').notNullable();
     table
       .uuid('cylinder_set')
       .references('id')
       .inTable('diving_cylinder_set')
       .notNullable();
+    table.integer('air_pressure').unsigned();
+    table.integer('oxygen_pressure').unsigned();
+    table.integer('helium_pressure').unsigned();
+    table.integer('argon_pressure').unsigned();
+    table.integer('price').unsigned(); // euro cents
     table.string('info', 2048); // arbitrary number
-    table.timestamps(true, true);
-  });
-
-  await knex.schema.createTable('fill_cylinder_event', (table) => {
-    table
-      .increments('fill_event')
-      .references('id')
-      .inTable('fill_event')
-      .notNullable();
-    table
-      .uuid('cylinder')
-      .references('id')
-      .inTable('diving_cylinder')
-      .notNullable();
-    table
-      .string('gas', 15)
-      .checkIn(['pressurized_air', 'oxygen', 'helium', 'argon'])
-      .notNullable();
-    table.integer('fill_pressure').unsigned().notNullable();
-    table.integer('cost').unsigned().notNullable(); // cost in euro cents
-    table.unique(['fill_event', 'cylinder']);
     table.timestamps(true, true);
   });
 }
 
 export async function down(knex: Knex): Promise<void> {
-  await knex.schema.dropTable('fill_cylinder_event');
-  await knex.schema.dropTable('fill_event');
+  await knex.schema.dropTableIfExists('fill_event');
 }
