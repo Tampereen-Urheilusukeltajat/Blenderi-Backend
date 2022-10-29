9e7297fd1dd2d23303e6bb78d198313cda6e9373
toppeh <38320587+toppeh@users.noreply.github.com>
2022-10-29 15:26:15 +0300

    Create table for a cylinder fill event (#28)

---
+++ src/database/migrations/20221025150522_create_fill_event_table.ts
@@ -0,0 +1,25 @@
+import { Knex } from 'knex';
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.createTable('fill_event', (table) => {
+    table.uuid('id').primary();
+    table.uuid('user').references('id').inTable('user').notNullable();
+    table
+      .uuid('cylinder_set')
+      .references('id')
+      .inTable('diving_cylinder_set')
+      .notNullable();
+    table.integer('air_pressure').unsigned().notNullable().defaultTo(0);
+    table.integer('oxygen_pressure').unsigned().notNullable().defaultTo(0);
+    table.integer('helium_pressure').unsigned().notNullable().defaultTo(0);
+    table.integer('argon_pressure').unsigned().notNullable().defaultTo(0);
+    table.integer('diluent_pressure').unsigned().notNullable().defaultTo(0);
+    table.integer('price').unsigned(); // euro cents
+    table.string('info', 1024); // magic number
+    table.timestamps(true, true);
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.dropTableIfExists('fill_event');
+}
