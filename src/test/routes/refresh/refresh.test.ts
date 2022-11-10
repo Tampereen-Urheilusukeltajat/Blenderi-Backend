a5303656dcae085d9249a23e9591f5e66c9fe263
Ile <ilmari.marttila@gmail.com>
2022-11-10 23:43:01 +0200

    Test for refresh endpoint

---
+++ src/test/routes/refresh/refresh.test.ts
@@ -0,0 +1,93 @@
+import {
+  describe,
+  test,
+  expect,
+  beforeAll,
+  afterAll,
+  beforeEach,
+} from '@jest/globals';
+import { FastifyInstance } from 'fastify';
+import { knexController } from '../../../database/database';
+import { buildServer } from '../../../server';
+import { createTestDatabase, dropTestDatabase } from '../../../lib/testUtils';
+
+describe('Refresh', () => {
+  const getTestInstance = async (): Promise<FastifyInstance> =>
+    buildServer({
+      routePrefix: 'api',
+    });
+
+  beforeAll(async () => {
+    await createTestDatabase('auth');
+  });
+
+  afterAll(async () => {
+    await dropTestDatabase();
+    await knexController.destroy();
+  });
+
+  let refreshToken: string;
+  let server;
+  beforeEach(async () => {
+    server = await getTestInstance();
+    const res = await server.inject({
+      url: '/api/login',
+      method: 'POST',
+      payload: {
+        email: 'user@example.com',
+        password: 'password',
+      },
+    });
+    expect(res.statusCode).toEqual(200);
+    const tokens = JSON.parse(res.body);
+    refreshToken = String(tokens.refreshToken);
+  });
+
+  describe('Happy path', () => {
+    test('It returns 200 on successful refresh token rotate', async () => {
+      const res = await server.inject({
+        url: '/api/refresh',
+        method: 'POST',
+        payload: {
+          refreshToken,
+        },
+      });
+      expect(res.statusCode).toEqual(200);
+      const resBody = JSON.parse(res.body);
+      expect(resBody).toHaveProperty('accessToken');
+      expect(resBody).toHaveProperty('refreshToken');
+      expect(resBody.refreshToken).not.toEqual(refreshToken);
+    });
+  });
+  describe('Unhappy path', () => {
+    test('It returns 401 for invalid refreshToken (tampered)', async () => {
+      const res = await server.inject({
+        url: '/api/refresh',
+        method: 'POST',
+        payload: {
+          refreshToken:
+            'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhIjpudWxsfQ.d3KrYIOoi5LIdMEbSpeMj7Hrw26hhBk0s9_FUwNTcoE',
+        },
+      });
+
+      expect(res.statusCode).toEqual(401);
+      const resBody = JSON.parse(res.body);
+      expect(resBody).not.toHaveProperty('accessToken');
+      expect(resBody).not.toHaveProperty('refreshToken');
+    });
+
+    test('It returns 400 for invalid body', async () => {
+      const res = await server.inject({
+        url: '/api/refresh',
+        method: 'POST',
+        payload: {
+          refreshToken: null,
+        },
+      });
+      expect(res.statusCode).toEqual(400);
+      const resBody = JSON.parse(res.body);
+      expect(resBody).not.toHaveProperty('accessToken');
+      expect(resBody).not.toHaveProperty('refreshToken');
+    });
+  });
+});
