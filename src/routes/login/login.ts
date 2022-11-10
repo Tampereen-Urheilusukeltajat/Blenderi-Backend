9680b55415e255dfc50cbb48f6160de1e1e0a13a
Ile <ilmari.marttila@gmail.com>
2022-11-10 10:50:26 +0200

    Types and documentation for login

--- src/routes/login/login.ts
+++ src/routes/login/login.ts
@@ -1,4 +1,4 @@
-import { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify';
+import { FastifyInstance, FastifyReply } from 'fastify';
 import { knexController } from '../../database/database';
 import { passwordIsValid } from '../../lib/auth';
 import { v4 as uuid } from 'uuid';
@@ -8,46 +8,43 @@ import {
   REFRESH_TOKEN_EXPIRE_TIME,
   ACCESS_TOKEN_EXPIRE_TIME,
 } from '../../lib/jwtUtils';
+import {
+  loginRequestBody,
+  LoginRequest,
+  authResponse,
+} from '../../types/auth.types';
 
 const schema = {
-  description: 'login',
   tags: ['Auth'],
-  body: {
-    type: 'object',
-    properties: {
-      email: { type: 'string' },
-      password: { type: 'string' },
-    },
-    required: ['email', 'password'],
-  },
+  body: loginRequestBody,
   response: {
-    200: {
-      description: 'Logged in',
-      type: 'object',
-      properties: {
-        accessToken: { type: 'string' },
-        refreshToken: { type: 'string' },
-      },
-    },
+    200: authResponse,
     400: { $ref: 'error' },
-    403: { $ref: 'error' },
+    401: { $ref: 'error' },
     500: { $ref: 'error' },
   },
 };
 
+schema.body.properties.email.example = 'john.doe@example.com';
+schema.body.properties.password.example = 'rockyou.txt';
+schema.response['200'].properties.accessToken.example =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ2YWx1ZSI6ImFjY2Vzc1Rva2VuIGV4YW1wbGUifQ.RhzwDe3kBpP4ZbwTXiSN3tQ9KtdZ6hGJwHdy9cJtAjo';
+schema.response['200'].properties.refreshToken.example =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ2YWx1ZSI6Im1lbmUgdMO2aWhpbiJ9.MRG6niUDfScxyj4p1NWh-0AJJlPjknC8wPTN2RM1d7g';
+
 const handler = async function (
-  request: FastifyRequest<{
-    Body: {
-      email: 'string';
-      password: 'string';
-    };
-  }>,
+  request: LoginRequest,
   reply: FastifyReply
 ): Promise<void> {
   const result: { id: 'String'; salt: 'String'; password_hash: 'String' } =
     await knexController('user')
       .where('email', request.body.email)
       .first('id', 'salt', 'password_hash');
+
+  if (result === undefined) {
+    // This means that user doesn't exist, we are not going to tell that to the client.
+    return errorHandler(reply, 401);
+  }
 
   if (
     !(await passwordIsValid(
---
+++ src/types/auth.types.ts
@@ -0,0 +1,20 @@
+import { Type, Static } from '@sinclair/typebox';
+import { FastifyRequest } from 'fastify';
+
+export const authResponse = Type.Object({
+  refreshToken: Type.String(),
+  accessToken: Type.String(),
+});
+
+export type AuthResponse = Static<typeof authResponse>;
+
+export const loginRequestBody = Type.Object({
+  email: Type.String({ minLength: 3, maxLength: 254, pattern: '^.+@....+$' }),
+  password: Type.String({ minLength: 8, maxLength: 1000 }),
+});
+
+export type LoginRequestBody = Static<typeof loginRequestBody>;
+
+export type LoginRequest = FastifyRequest<{
+  Body: LoginRequestBody;
+}>;
