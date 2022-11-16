c0978c423b64e47657b433d6c4896a342b713ed3
toppeh <38320587+toppeh@users.noreply.github.com>
2022-11-16 21:02:04 +0200

    Added database tables for gas prices (#29)

---
+++ src/database/migrations/20221101124519_create_prices_table.ts
@@ -0,0 +1,16 @@
+import { Knex } from 'knex';
+/* 
+  Used currency is euros (€) and the price is saved in euro cents per litre of gas (100 cents = 1 €).
+*/
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.createTable('prices', (table) => {
+    table.enu('gas', ['oxygen', 'helium', 'argon']).notNullable();
+    table.integer('price_per_litre_in_eur_cents').unsigned().notNullable();
+    table.uuid('admin').references('id').inTable('user').notNullable();
+    table.timestamps(true, true);
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.dropTableIfExists('prices');
+}
