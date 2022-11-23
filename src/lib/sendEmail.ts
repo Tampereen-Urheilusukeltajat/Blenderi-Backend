c867e3987e37894c7dd815d4f07c81e8d2ce2d01
Kimi <porthan.kimi@gmail.com>
2022-11-23 15:03:54 +0200

    move env variable init out from func

--- src/lib/sendEmail.ts
+++ src/lib/sendEmail.ts
@@ -2,28 +2,42 @@ import sgMail from '@sendgrid/mail';
 import { emailMsg } from '../types/email.types';
 import { log } from './log';
 
-// return true if email was sent succesfully.
-export const sendEmail = async (msg: emailMsg): Promise<boolean> => {
-  const apiKey: string | undefined = process.env.SENDGRID_API_KEY;
-  const from: string | undefined = process.env.SENDGRID_FROM_EMAIL;
+const API_KEY: string | undefined = process.env.SENDGRID_API_KEY;
+const FROM_EMAIL: string | undefined = process.env.SENDGRID_FROM_EMAIL;
 
-  if (apiKey === undefined || from === undefined) {
+/* 
+Return true if email was sent succesfully. 
+Use second sandbox = true in tests, so no real emails are sent.
+*/
+const sendEmail = (msg: emailMsg, sandbox = false): boolean => {
+  if (API_KEY === undefined || FROM_EMAIL === undefined) {
     log.error('sendEmail function has no API key or "from" address!');
     return false;
   }
-  sgMail.setApiKey(apiKey);
+  sgMail.setApiKey(API_KEY);
 
-  try {
-    const response = await sgMail.send({ ...msg, from });
+  void (async () => {
+    try {
+      // Request body for sending the email.
+      const reqBody = {
+        ...msg,
+        from: FROM_EMAIL,
+        mailSettings: { sandboxMode: { enable: sandbox } },
+      };
 
-    if (response[0].statusCode !== 202) {
-      log.error('Sending email failed. StatusCode:', response[0].statusCode);
+      const response = await sgMail.send(reqBody);
+
+      // 200 on sandbox, 202 on real email.
+      if (response[0].statusCode !== 202 && response[0].statusCode !== 200) {
+        return false;
+      }
+    } catch (error) {
+      log.error('Error when sending email. Error:', error);
       return false;
     }
-  } catch (error) {
-    log.error('Error when sending email. Error:', error);
-    return false;
-  }
+  })();
 
   return true;
 };
+
+export default sendEmail;
