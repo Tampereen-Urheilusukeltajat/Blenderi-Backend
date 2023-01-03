b5c49df22eeb2829252fafbb339733f6a4136840
Akseli Kolari <33838340+Akzuu@users.noreply.github.com>
2023-01-03 17:10:08 +0200

    Update jest.setEnvVariables.js
    Co-authored-by: ilesoft <45848534+ilesoft@users.noreply.github.com>

--- jest.setEnvVariables.js
+++ jest.setEnvVariables.js
@@ -1,6 +1,5 @@
 /* eslint-disable */
 import { randomUUID } from 'crypto';
 
-const TEST_DB = `test-db-${randomUUID()}`;
-process.env.TEST_DATABASE = TEST_DB;
+process.env.TEST_DATABASE = `test-db-${randomUUID()}`;
 
