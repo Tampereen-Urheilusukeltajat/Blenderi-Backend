510d75e5876474a0d1a774f5837b7b623d5d2921
Ile <ilmari.marttila@gmail.com>
2023-01-12 09:19:25 +0200

    Add simple tests

--- src/lib/sendEmail.ts
+++ src/lib/sendEmail.ts
@@ -4,36 +4,33 @@ import { emailMsg } from '../types/email.types';
 const API_KEY: string | undefined = process.env.SENDGRID_API_KEY;
 const FROM_EMAIL: string | undefined = process.env.SENDGRID_FROM_EMAIL;
 
-/* 
-Use param sandbox = true in tests, so no real emails are sent.
-*/
-const sendEmail = async (msg: emailMsg, sandbox = false): Promise<void> => {
-  if (API_KEY === undefined || FROM_EMAIL === undefined) {
-    throw new Error('sendEmail function has no API key or "from" address!');
-  }
-  sgMail.setApiKey(API_KEY);
+if (API_KEY === undefined || FROM_EMAIL === undefined) {
+  throw new Error('sendEmail function has no API key or "from" address!');
+}
 
+sgMail.setApiKey(API_KEY);
+
+const sendEmail = async (msg: emailMsg): Promise<void> => {
   try {
     const requestEmailBody = {
       ...msg,
       from: FROM_EMAIL,
-      mailSettings: { sandboxMode: { enable: sandbox } },
+      mailSettings: {},
     };
 
     const response = await sgMail.send(requestEmailBody);
 
-    // 200 on sandbox, 202 on real email.
-    if (response[0].statusCode !== 202 && response[0].statusCode !== 200) {
+    if (response[0].statusCode !== 202) {
       throw new Error(
-        `SendGrid returned statusCode ${response[0].statusCode}.`
+        `SendGrid returned statusCode ${String(response[0].statusCode)}.`
       );
     }
   } catch (error) {
-    let message = 'Unknown Error on sendEmail';
+    let message: string;
     if (error instanceof Error) message = error.message;
     else message = String(error);
 
-    throw new Error(`Error when sending email. Error: ${message}.`);
+    throw new Error(`Error when sending email. Error: ${message}`);
   }
 };
 
