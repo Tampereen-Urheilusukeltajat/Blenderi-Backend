8068968a85ede5688387f88901f370368bcf93a9
Akseli Kolari <akzuu@pm.me>
2023-02-19 14:38:03 +0200

    Final fixes to the migration

--- src/database/migrations/20230219113418_modify_diving_cylinder_set.ts
+++ src/database/migrations/20230219113418_modify_diving_cylinder_set.ts
@@ -3,7 +3,7 @@ import { Knex } from 'knex';
 export async function up(knex: Knex): Promise<void> {
   await knex.raw(`
     ALTER TABLE diving_cylinder_set
-    ADD archived BOOLEAN DEFAULT 0;
+    ADD archived BOOLEAN DEFAULT 0 NOT NULL;
   `);
 }
 
