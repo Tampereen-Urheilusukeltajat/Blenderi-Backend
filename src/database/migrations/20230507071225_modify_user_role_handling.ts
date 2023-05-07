33ae4eb6a120af4ad82f5efee39ffc559069d885
Akseli Kolari <akzuu@pm.me>
2023-05-07 10:22:50 +0300

    Required database changes

---
+++ src/database/migrations/20230507071225_modify_user_role_handling.ts
@@ -0,0 +1,42 @@
+import { Knex } from 'knex';
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.raw(`
+    ALTER TABLE user
+    DROP COLUMN IF EXISTS is_blender;
+  `);
+
+  await knex.raw(`
+    ALTER TABLE user
+    DROP COLUMN IF EXISTS is_admin;
+  `);
+
+  await knex.raw(`
+    RENAME TABLE IF EXISTS initial_blender_access_list TO access_role_list;
+  `);
+
+  await knex.raw(`
+    ALTER TABLE access_role_list
+    ADD COLUMN is_admin BOOLEAN NOT NULL DEFAULT 0;
+  `);
+
+  await knex.raw(`
+    ALTER TABLE access_role_list
+    ADD COLUMN is_advanced_blender BOOLEAN NOT NULL DEFAULT 0;
+    `);
+
+  await knex.raw(`
+    ALTER TABLE access_role_list
+    ADD COLUMN is_blender BOOLEAN NOT NULL DEFAULT 0;
+
+    `);
+
+  await knex.raw(`
+    ALTER TABLE access_role_list
+    ADD COLUMN is_instructor BOOLEAN NOT NULL DEFAULT 0;
+    `);
+}
+
+export async function down(knex: Knex): Promise<void> {
+  // noop
+}
