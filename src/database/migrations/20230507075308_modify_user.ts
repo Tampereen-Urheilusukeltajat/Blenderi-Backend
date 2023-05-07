8f23aa6204e5dce4ead6977434440e4143be1fa0
Akseli Kolari <akzuu@pm.me>
2023-05-07 11:10:18 +0300

    Phone -> phone number

---
+++ src/database/migrations/20230507075308_modify_user.ts
@@ -0,0 +1,12 @@
+import { Knex } from 'knex';
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.raw(`
+    ALTER TABLE user
+    RENAME COLUMN phone TO phone_number;
+  `);
+}
+
+export async function down(knex: Knex): Promise<void> {
+  // noop
+}
