ddf65805279ed0d605cda768e59889b93d85c9bc
Akseli Kolari <33838340+Akzuu@users.noreply.github.com>
2023-05-20 13:20:57 +0300

    Save last login to the database (#77)

---
+++ src/database/migrations/20230520074945_add_last_login.ts
@@ -0,0 +1,12 @@
+import { Knex } from 'knex';
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.raw(`
+    ALTER TABLE user
+    ADD COLUMN last_login datetime NOT NULL DEFAULT NOW();
+  `);
+}
+
+export async function down(knex: Knex): Promise<void> {
+  // noop
+}
--- src/lib/queries/user.ts
+++ src/lib/queries/user.ts
@@ -76,3 +76,16 @@ export const updateUser = async (
   await transaction.commit();
   return editedUser;
 };
+
+export const updateLastLogin = async (
+  userId: string,
+  trx?: Knex.Transaction
+): Promise<void> => {
+  const db = trx ?? knexController;
+
+  const alteredRows = await db('user')
+    .where({ id: userId })
+    .update({ last_login: new Date(Date.now()) });
+
+  if (alteredRows !== 1) throw new Error('Updating last login failed');
+};
