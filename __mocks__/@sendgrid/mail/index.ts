e3a042146793ae5eeaf7acafb884fab97d2bb041
Ile <ilmari.marttila@gmail.com>
2023-05-29 21:52:24 +0300

    Endpoint and e2e test for password reset

--- __mocks__/@sendgrid/mail/index.ts
+++ __mocks__/@sendgrid/mail/index.ts
@@ -1,5 +1,26 @@
 import { jest, expect } from '@jest/globals';
 
+type WaitForMeMock = jest.Mock & {
+  waitUntilComplete: () => Promise<void>;
+};
+
+export const waitForMeFactory = (): WaitForMeMock => {
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  let _resolve: Function;
+  const promise = new Promise<void>((resolve) => (_resolve = resolve));
+
+  const mock = jest.fn(() => {
+    _resolve();
+  }) as WaitForMeMock; // force casting
+
+  mock.waitUntilComplete = async () => promise;
+
+  return mock;
+};
+
+let message: undefined | string;
+let globalWaiter: WaitForMeMock;
+
 export default {
   setApiKey: jest.fn((key): void => {
     expect(key).toBeTruthy();
@@ -9,6 +30,23 @@ export default {
     if (data.to === 'invalid') {
       return [{ statusCode: 500 }];
     }
+
+    message = data.text;
+
+    if (globalWaiter !== undefined) {
+      globalWaiter();
+    }
+
     return [{ statusCode: 202 }];
+  },
+
+  setMailWaiter(): void {
+    globalWaiter = waitForMeFactory();
+  },
+
+  async getMessage(): Promise<string> {
+    await globalWaiter.waitUntilComplete();
+    if (message === undefined) throw Error('Email mock is broken');
+    return message;
   },
 };
---
+++ src/test/routes/password-reset/end-to-end-password-reset.test.ts
@@ -0,0 +1,144 @@
+import { describe, expect, test } from '@jest/globals';
+import { knexController } from '../../../database/database';
+import { buildServer } from '../../../server';
+import {
+  createTestDatabase,
+  dropTestDatabase,
+  startRedisConnection,
+  stopRedisConnection,
+} from '../../../lib/utils/testUtils';
+
+// Mock for Send Grid
+import sgMail from '@sendgrid/mail';
+
+describe('Password can be changed', () => {
+  const longTestTimeOut = 10000; // ms
+  test(
+    'Password can be changed',
+    async () => {
+      const server = await buildServer({
+        routePrefix: 'api',
+      });
+
+      await createTestDatabase('password_reset');
+      await startRedisConnection();
+
+      // @ts-expect-error: getMessage is injected to mock function
+      sgMail.setMailWaiter();
+      const resetRequestResponse = await server.inject({
+        url: '/api/reset-password/reset-request',
+        method: 'POST',
+        payload: {
+          email: 'e2e.user@example.com',
+        },
+      });
+
+      expect(resetRequestResponse.statusCode).toEqual(202);
+
+      // @ts-expect-error: getMessage is injected to mock function
+      const requestMessage: string = await sgMail.getMessage();
+
+      const indexOfTokenString = requestMessage.indexOf('?token=') + 7;
+      const token = requestMessage.substring(
+        indexOfTokenString,
+        indexOfTokenString + 36
+      );
+
+      const indexOfUserIdString = requestMessage.indexOf('&id=') + 4;
+      const userId = requestMessage.substring(
+        indexOfUserIdString,
+        indexOfUserIdString + 36
+      );
+
+      // Try to reset with invalid token and login with it
+      const setPasswordResponseWithInvalidToken = await server.inject({
+        url: '/api/reset-password/set-password',
+        method: 'POST',
+        payload: {
+          token: '56387c3d-145c-44e6-a037-9541848bc757',
+          userId,
+          password: 'rockYou.csv',
+        },
+      });
+
+      expect(setPasswordResponseWithInvalidToken.statusCode).toEqual(204);
+
+      // Wait for possible unwanted password resetting to be done
+      await new Promise((resolve) => setTimeout(resolve, 5000));
+
+      // Login with old password => should be successful
+
+      const validLoginWithOldPasswordResponse = await server.inject({
+        url: '/api/login',
+        method: 'POST',
+        payload: {
+          email: 'e2e.user@example.com',
+          password: 'password',
+        },
+      });
+
+      expect(validLoginWithOldPasswordResponse.statusCode).toEqual(200);
+
+      // Try to log in with password that has been tried to be set
+
+      const invalidLoginWithInvalidlyChangedPassword = await server.inject({
+        url: '/api/login',
+        method: 'POST',
+        payload: {
+          email: 'e2e.user@example.com',
+          password: 'rockYou.csv',
+        },
+      });
+
+      expect(invalidLoginWithInvalidlyChangedPassword.statusCode).toEqual(401);
+
+      // @ts-expect-error: getMessage is injected to mock function
+      sgMail.setMailWaiter();
+
+      const setPasswordResponse = await server.inject({
+        url: '/api/reset-password/set-password',
+        method: 'POST',
+        payload: {
+          token,
+          userId,
+          password: 'rockYou.txt',
+        },
+      });
+
+      expect(setPasswordResponse.statusCode).toEqual(204);
+
+      // @ts-expect-error: getMessage is injected to mock function
+      const setMessage: string = await sgMail.getMessage();
+      expect(setMessage).toContain(
+        'Sait tämän viestin, koska olet vaihtanut salasanasi Tampereen Urheilusukeltajien Täyttöpaikka-palveluun.'
+      );
+
+      const oldPasswordLoginResponse = await server.inject({
+        url: '/api/login',
+        method: 'POST',
+        payload: {
+          email: 'e2e.user@example.com',
+          password: 'password',
+        },
+      });
+
+      expect(oldPasswordLoginResponse.statusCode).toEqual(401);
+
+      const validLoginResponse = await server.inject({
+        url: '/api/login',
+        method: 'POST',
+        payload: {
+          email: 'e2e.user@example.com',
+          password: 'rockYou.txt',
+        },
+      });
+
+      expect(validLoginResponse.statusCode).toEqual(200);
+
+      await dropTestDatabase();
+      await knexController.destroy();
+      await stopRedisConnection();
+    },
+    longTestTimeOut
+  );
+});
