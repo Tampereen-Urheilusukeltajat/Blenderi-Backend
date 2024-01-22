4bd866ebf6ec3725ec0032975f43653357b91eee
Akseli Kolari <33838340+Akzuu@users.noreply.github.com>
2024-01-22 12:11:47 +0200

    Fix invalid fill_event_gas_fill primary key  (#100)
    Fill event gas fill had invalid primary key, which denied user from creating a fill event with two different storage cylinders that held the same gas. Use case is that you fill e.g. oxygen from storage cylinder 1 and it dries out and then you fill little bit more from storage cylinder 2.

---
+++ src/database/migrations/20240119142857_fix_fill_event_gas_fill_primary_key.ts
@@ -0,0 +1,14 @@
+import { Knex } from 'knex';
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.raw(`SET foreign_key_checks = 0;`);
+  await knex.raw(`ALTER TABLE fill_event_gas_fill DROP PRIMARY KEY;`);
+  await knex.raw(
+    'ALTER TABLE fill_event_gas_fill ADD COLUMN id UUID DEFAULT UUID() NOT NULL PRIMARY KEY;'
+  );
+  await knex.raw(`SET foreign_key_checks = 1;`);
+}
+
+export async function down(): Promise<void> {
+  // noop
+}
