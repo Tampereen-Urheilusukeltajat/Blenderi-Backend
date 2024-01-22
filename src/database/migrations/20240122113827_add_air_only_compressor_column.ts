6953e3e2a3a7e68ddec9a8450ef48534792d4af3
Akseli Kolari <akzuu@pm.me>
2024-01-22 13:41:24 +0200

    compressor air only column

---
+++ src/database/migrations/20240122113827_add_air_only_compressor_column.ts
@@ -0,0 +1,12 @@
+import { Knex } from 'knex';
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.raw(`
+    ALTER TABLE compressor
+    ADD COLUMN air_only BOOLEAN DEFAULT 1 NOT NULL
+  `);
+}
+
+export async function down(): Promise<void> {
+  // noop
+}
